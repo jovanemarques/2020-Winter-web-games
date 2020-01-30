44cd272c8ff082caa7a21c06ff42daa45e47fcc5
Jovane Marques <jovanemarques@gmail.com>
2020-01-30 01:13:17 -0500

    adding the winning functions

--- Assign1/jovane-slotmachine/scripts/game.js
+++ Assign1/jovane-slotmachine/scripts/game.js
@@ -5,6 +5,13 @@
     var reel_1;
     var reel_2;
     var reel_3;
+    var results = [];
+    var winnings = 0;
+    var playerBet = 10;
+    var playerMoney = 1000;
+    var jackpot = 5000;
+    var winNumber = 0;
+    var lossNumber = 0;
     var themes = [
         {
             name: 'theme1',
@@ -18,14 +25,14 @@
                 pos_reel_2: { x: 267, y: 85 },
                 pos_reel_3: { x: 388, y: 85 },
                 images: [
+                    './assets/images/reel-blank.png',
                     './assets/images/reel-bar.png',
                     './assets/images/reel-cherry.png',
-                    './assets/images/reel-7.png',
                     './assets/images/reel-dollar.png',
                     './assets/images/reel-star.png',
-                    './assets/images/reel-diamond.png',
                     './assets/images/reel-watermelon.png',
-                    './assets/images/reel-lemon.png'
+                    './assets/images/reel-lemon.png',
+                    './assets/images/reel-7.png',
                 ]
             }
         },
@@ -41,14 +48,14 @@
                 pos_reel_2: { x: 270, y: 145 },
                 pos_reel_3: { x: 435, y: 145 },
                 images: [
+                    './assets/images/reel-blank.png',
                     './assets/images/reel-bar.png',
                     './assets/images/reel-cherry.png',
-                    './assets/images/reel-7.png',
                     './assets/images/reel-dollar.png',
                     './assets/images/reel-star.png',
-                    './assets/images/reel-diamond.png',
                     './assets/images/reel-watermelon.png',
-                    './assets/images/reel-lemon.png'
+                    './assets/images/reel-lemon.png',
+                    './assets/images/reel-7.png',
                 ]
             }
         },
@@ -64,14 +71,14 @@
                 pos_reel_2: { x: 267, y: 85 },
                 pos_reel_3: { x: 388, y: 85 },
                 images: [
+                    './assets/images/reel-blank.png',
                     './assets/images/reel-bar.png',
                     './assets/images/reel-cherry.png',
-                    './assets/images/reel-7.png',
                     './assets/images/reel-dollar.png',
                     './assets/images/reel-star.png',
-                    './assets/images/reel-diamond.png',
                     './assets/images/reel-watermelon.png',
-                    './assets/images/reel-lemon.png'
+                    './assets/images/reel-lemon.png',
+                    './assets/images/reel-7.png',
                 ]
             }
         }
@@ -99,11 +106,39 @@
     function Reels() {
         var outCome = [0, 0, 0];
         var betLine = [];
-        var results = themes[theme].reels.images.map(function (e) { return { item: e, total: 0 }; });
+        results = themes[theme].reels.images.map(function (e) { return { item: e, total: 0 }; });
         for (var spin = 0; spin < 3; spin++) {
             outCome[spin] = Math.floor((Math.random() * 65) + 1);
             switch (outCome[spin]) {
-                case checkRange(outCome[spin], 1, 65):
+                case checkRange(outCome[spin], 1, 27):
+                    betLine[spin] = 0;
+                    results[0].total++;
+                    break;
+                case checkRange(outCome[spin], 28, 37):
+                    betLine[spin] = 1;
+                    results[1].total++;
+                    break;
+                case checkRange(outCome[spin], 38, 46):
+                    betLine[spin] = 2;
+                    results[2].total++;
+                    break;
+                case checkRange(outCome[spin], 47, 54):
+                    betLine[spin] = 3;
+                    results[3].total++;
+                    break;
+                case checkRange(outCome[spin], 55, 59):
+                    betLine[spin] = 4;
+                    results[4].total++;
+                    break;
+                case checkRange(outCome[spin], 60, 62):
+                    betLine[spin] = 5;
+                    results[5].total++;
+                    break;
+                case checkRange(outCome[spin], 63, 64):
+                    betLine[spin] = 6;
+                    results[6].total++;
+                    break;
+                case checkRange(outCome[spin], 65, 65):
                     betLine[spin] = 7;
                     results[7].total++;
                     break;
@@ -149,7 +184,89 @@
             reel_3.x = themes[theme].reels.pos_reel_3.x;
             reel_3.y = themes[theme].reels.pos_reel_3.y;
             stage.addChild(reel_3);
+            determineWinnings();
         });
+    }
+    function determineWinnings() {
+        if (results[0].total === 0) {
+            if (results[1].total === 3) {
+                winnings = playerBet * 10;
+            }
+            else if (results[2].total === 3) {
+                winnings = playerBet * 20;
+            }
+            else if (results[3].total === 3) {
+                winnings = playerBet * 30;
+            }
+            else if (results[4].total === 3) {
+                winnings = playerBet * 40;
+            }
+            else if (results[5].total === 3) {
+                winnings = playerBet * 50;
+            }
+            else if (results[6].total === 3) {
+                winnings = playerBet * 75;
+            }
+            else if (results[7].total === 3) {
+                winnings = playerBet * 100;
+            }
+            else if (results[1].total === 2) {
+                winnings = playerBet * 2;
+            }
+            else if (results[2].total === 2) {
+                winnings = playerBet * 2;
+            }
+            else if (results[3].total === 2) {
+                winnings = playerBet * 3;
+            }
+            else if (results[4].total === 2) {
+                winnings = playerBet * 4;
+            }
+            else if (results[5].total === 2) {
+                winnings = playerBet * 5;
+            }
+            else if (results[6].total === 2) {
+                winnings = playerBet * 10;
+            }
+            else if (results[7].total === 2) {
+                winnings = playerBet * 20;
+            }
+            else if (results[7].total === 1) {
+                winnings = playerBet * 5;
+            }
+            else {
+                winnings = playerBet * 1;
+            }
+            winNumber++;
+            showWinMessage(winnings);
+        }
+        else {
+            lossNumber++;
+            showLossMessage();
+        }
+    }
+    function showWinMessage(winnings) {
+        playerMoney += winnings;
+        resetFruitTally();
+        checkJackPot();
+        alert('You Won: $' + winnings);
+    }
+    function checkJackPot() {
+        var jackPotTry = Math.floor(Math.random() * 51 + 1);
+        var jackPotWin = Math.floor(Math.random() * 51 + 1);
+        if (jackPotTry == jackPotWin) {
+            alert("You Won the $" + jackpot + " Jackpot!!");
+            playerMoney += jackpot;
+            jackpot = 1000;
+        }
+    }
+    function showLossMessage() {
+        playerMoney -= playerBet;
+        resetFruitTally();
+        alert('You Lost!');
+    }
+    function resetFruitTally() {
+        results.forEach(function (r) { return r.total = 0; });
     }
     window.addEventListener("load", Start);
 })();
--- Assign1/jovane-slotmachine/scripts/game.ts
+++ Assign1/jovane-slotmachine/scripts/game.ts
@@ -1,9 +1,18 @@
 (function () {
   let canvas = document.getElementsByTagName('canvas')[0];
   let stage: createjs.Stage;
+  //TODO: remove hard coded reel number
   let reel_1: createjs.Bitmap;
   let reel_2: createjs.Bitmap;
   let reel_3: createjs.Bitmap;
+  let results: { item: string; total: number; }[] = [];
+  let winnings: number = 0;
+  let playerBet: number = 10;
+  let playerMoney: number = 1000;
+  let jackpot: number = 5000;
+  let winNumber: number = 0;
+  let lossNumber: number = 0;
+
   let themes = [
     {
       name:'theme1',
@@ -17,14 +26,15 @@
         pos_reel_2:{x:267,   y:85},
         pos_reel_3:{x:388,   y:85},
         images:[
+          './assets/images/reel-blank.png', 
           './assets/images/reel-bar.png', 
           './assets/images/reel-cherry.png', 
-          './assets/images/reel-7.png',
           './assets/images/reel-dollar.png',
           './assets/images/reel-star.png',
-          './assets/images/reel-diamond.png',
+          //'./assets/images/reel-diamond.png',
           './assets/images/reel-watermelon.png',
-          './assets/images/reel-lemon.png'
+          './assets/images/reel-lemon.png',
+          './assets/images/reel-7.png',
         ]
       }
     },
@@ -40,14 +50,15 @@
         pos_reel_2:{x:270,y:145},
         pos_reel_3:{x:435,y:145},
         images:[
+          './assets/images/reel-blank.png', 
           './assets/images/reel-bar.png', 
           './assets/images/reel-cherry.png', 
-          './assets/images/reel-7.png',
           './assets/images/reel-dollar.png',
           './assets/images/reel-star.png',
-          './assets/images/reel-diamond.png',
+          //'./assets/images/reel-diamond.png',
           './assets/images/reel-watermelon.png',
-          './assets/images/reel-lemon.png'
+          './assets/images/reel-lemon.png',
+          './assets/images/reel-7.png',
         ]
       }
     },
@@ -62,15 +73,16 @@
         pos_reel_1:{x:142.5, y:85},
         pos_reel_2:{x:267,   y:85},
         pos_reel_3:{x:388,   y:85},
-        images:[ 
+        images:[
+          './assets/images/reel-blank.png', 
           './assets/images/reel-bar.png', 
           './assets/images/reel-cherry.png', 
-          './assets/images/reel-7.png',
           './assets/images/reel-dollar.png',
           './assets/images/reel-star.png',
-          './assets/images/reel-diamond.png',
+          //'./assets/images/reel-diamond.png',
           './assets/images/reel-watermelon.png',
-          './assets/images/reel-lemon.png'
+          './assets/images/reel-lemon.png',
+          './assets/images/reel-7.png',
         ]
       }
     }
@@ -113,47 +125,43 @@
     let outCome: Array<number> = [0, 0, 0];
     let betLine:Array<number> = [];
     //TODO: check if it right to use objects like this in typescript
-    let results = themes[theme].reels.images.map(e => {return {item:e, total:0}});
+    results = themes[theme].reels.images.map(e => {return {item:e, total:0}});
     //TODO: convert to an array of reels to be easy to enter more reels
     for (let spin:number = 0; spin < 3; spin++) {
       outCome[spin] = Math.floor((Math.random() * 65) + 1);
       switch (outCome[spin]) {
-          case checkRange(outCome[spin], 1, 65):  // 41.5% probability
-              betLine[spin] = 7//themes[theme].reels.images[0];
-              results[7].total++;
-              break;
-          // case checkRange(outCome[spin], 1, 27):  // 41.5% probability
-          //     betLine[spin] = themes[theme].reels.images[0];
-          //     results[];
-          //     break;
-          // case checkRange(outCome[spin], 28, 37): // 15.4% probability
-          //     betLine[spin] = "Grapes";
-          //     grapes++;
-          //     break;
-          // case checkRange(outCome[spin], 38, 46): // 13.8% probability
-          //     betLine[spin] = "Banana";
-          //     bananas++;
-          //     break;
-          // case checkRange(outCome[spin], 47, 54): // 12.3% probability
-          //     betLine[spin] = "Orange";
-          //     oranges++;
-          //     break;
-          // case checkRange(outCome[spin], 55, 59): //  7.7% probability
-          //     betLine[spin] = "Cherry";
-          //     cherries++;
-          //     break;
-          // case checkRange(outCome[spin], 60, 62): //  4.6% probability
-          //     betLine[spin] = "Bar";
-          //     bars++;
-          //     break;
-          // case checkRange(outCome[spin], 63, 64): //  3.1% probability
-          //     betLine[spin] = "Bell";
-          //     bells++;
-          //     break;
-          // case checkRange(outCome[spin], 65, 65): //  1.5% probability
-          //     betLine[spin] = "Seven";
-          //     sevens++;
-          //     break;
+        case checkRange(outCome[spin], 1, 27):  // 41.5% probability
+          betLine[spin] = 0//themes[theme].reels.images[0];
+          results[0].total++;
+          break;
+        case checkRange(outCome[spin], 28, 37): // 15.4% probability
+          betLine[spin] = 1;
+          results[1].total++;
+          break;
+        case checkRange(outCome[spin], 38, 46): // 13.8% probability
+          betLine[spin] = 2;
+          results[2].total++;
+          break;
+        case checkRange(outCome[spin], 47, 54): // 12.3% probability
+            betLine[spin] = 3;
+            results[3].total++;
+            break;
+        case checkRange(outCome[spin], 55, 59): //  7.7% probability
+            betLine[spin] = 4;
+            results[4].total++;
+            break;
+        case checkRange(outCome[spin], 60, 62): //  4.6% probability
+            betLine[spin] = 5;
+            results[5].total++;
+            break;
+        case checkRange(outCome[spin], 63, 64): //  3.1% probability
+            betLine[spin] = 6;
+            results[6].total++;
+            break;
+        case checkRange(outCome[spin], 65, 65): //  1.5% probability
+            betLine[spin] = 7;
+            results[7].total++;
+            break;
       }
     }
     return betLine;
@@ -209,10 +217,116 @@
       reel_3.x = themes[theme].reels.pos_reel_3.x;
       reel_3.y = themes[theme].reels.pos_reel_3.y;
       stage.addChild(reel_3);
+
+      determineWinnings();
     });
   }
 
-  
+  /**
+   * This function calculates the player's winnings, if any
+   */ 
+  function determineWinnings():void
+  {
+    if (results[0].total === 0)
+    {
+        if (results[1].total === 3){ 
+          winnings = playerBet * 10;
+        }
+        else if (results[2].total === 3){
+            winnings = playerBet * 20;
+        }
+        else if (results[3].total === 3){
+            winnings = playerBet * 30;
+        }
+        else if (results[4].total === 3){
+            winnings = playerBet * 40;
+        }
+        else if (results[5].total === 3){
+            winnings = playerBet * 50;
+        }
+        else if (results[6].total === 3){
+            winnings = playerBet * 75;
+        }
+        else if (results[7].total === 3){
+            winnings = playerBet * 100;
+        }
+        else if (results[1].total === 2){
+            winnings = playerBet * 2;
+        }
+        else if (results[2].total === 2){
+            winnings = playerBet * 2;
+        }
+        else if (results[3].total === 2){
+            winnings = playerBet * 3;
+        }
+        else if (results[4].total === 2){
+            winnings = playerBet * 4;
+        }
+        else if (results[5].total === 2){
+            winnings = playerBet * 5;
+        }
+        else if (results[6].total === 2){
+            winnings = playerBet * 10;
+        }
+        else if (results[7].total === 2){
+            winnings = playerBet * 20;
+        }
+        else if (results[7].total === 1){
+            winnings = playerBet * 5;
+        }
+        else {
+            winnings = playerBet * 1;
+        }
+        winNumber++;
+        showWinMessage(winnings);
+    }
+    else
+    {
+        lossNumber++;
+        showLossMessage();
+    }
+  }
+
+  /**
+   * Utility function to show a win message and increase player money
+   * @param winnings 
+   */
+  function showWinMessage(winnings:number):void {
+    playerMoney += winnings;
+    resetFruitTally();
+    checkJackPot();
+    //TODO: change the msg to a label
+    alert('You Won: $' + winnings);
+  }
+
+  /**
+   * Check to see if the player won the jackpot
+   */
+  function checkJackPot() {
+    /* compare two random values */
+    let jackPotTry:number = Math.floor(Math.random() * 51 + 1);
+    let jackPotWin:number = Math.floor(Math.random() * 51 + 1);
+    if (jackPotTry == jackPotWin) {
+        //TODO: change the msg to a label
+        alert("You Won the $" + jackpot + " Jackpot!!");
+        playerMoney += jackpot;
+        jackpot = 1000;
+    }
+  }
+
+  /**
+   * Utility function to show a loss message and reduce player money
+   */
+  function showLossMessage():void {
+    playerMoney -= playerBet;
+    resetFruitTally();
+    //TODO: change the msg to a label
+    alert('You Lost!');
+  }
+
+  function resetFruitTally():void {
+    results.forEach(r => r.total = 0);
+}
 
   window.addEventListener("load", Start);
 })();
