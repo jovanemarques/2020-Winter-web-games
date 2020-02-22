07b8faf9258a1087e32df0bc45c1eb9617f143b0
Jovane Marques <jovanemarques@gmail.com>
2020-02-22 11:05:10 -0500

    mid term - main requirements done + animation

--- COMP397-W2020-Midterm-Jovane/Scripts/scenes/Play.js
+++ COMP397-W2020-Midterm-Jovane/Scripts/scenes/Play.js
@@ -12,6 +12,12 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+/**
+ * Source File: Play.ts
+ * Author: Tom Tsiliopoulos / Jovane Marques
+ * Date: Feb 22, 2020
+ * Description: Play Scene of a dice game
+ */
 var scenes;
 (function (scenes) {
     var Play = /** @class */ (function (_super) {
@@ -20,6 +26,7 @@ var scenes;
         // CONSTRUCTOR
         function Play() {
             var _this = _super.call(this) || this;
+            _this._moveDices = false;
             _this._diceArray = [
                 config.Game.ASSETS.getResult("diceBlank"),
                 config.Game.ASSETS.getResult("dice1"),
@@ -33,28 +40,71 @@ var scenes;
             return _this;
         }
         // PRIVATE METHODS
+        /**
+         * Method to wait a period of time before doing some actions
+         *
+         * @private
+         * @param {number} time
+         * @returns
+         * @memberof Play
+         */
+        Play.prototype.sleep = function (time) {
+            return new Promise(function (resolve) { return setTimeout(resolve, time); });
+        };
         // PUBLIC METHODS
-        //initialize and instatiate
+        /**
+         * initialize and instatiate
+         *
+         * @memberof Play
+         */
         Play.prototype.Start = function () {
             var _this = this;
             this._dice1 = new objects.Button(config.Game.ASSETS.getResult("diceBlank"), 100, 150);
+            this._dice1Label = new objects.Label("0", "30px", void 0, void 0, 200, 360);
             this._dice2 = new objects.Button(config.Game.ASSETS.getResult("diceBlank"), 350, 150);
+            this._dice2Label = new objects.Label("0", "30px", void 0, void 0, 450, 360);
             this._rollBtn = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 250, 400);
             this._rollBtn.on('click', function (e) {
-                _this._dice1Number = Math.floor(util.Mathf.RandomRange(1, 6));
-                _this._dice1.image = _this._diceArray[_this._dice1Number];
-                console.log(_this._dice1Number);
-                _this._dice2Number = Math.floor(util.Mathf.RandomRange(1, 6));
-                _this._dice2.image = _this._diceArray[_this._dice2Number];
-                console.log(_this._dice2Number);
+                _this._moveDices = true;
+                _this.sleep(1000).then(function () {
+                    _this._dice1Number = Math.floor(util.Mathf.RandomRange(1, 6));
+                    _this._dice1.image = _this._diceArray[_this._dice1Number];
+                    _this._dice1Label.text = _this._dice1Number.toString();
+                    console.log(_this._dice1Number);
+                    _this._dice2Number = Math.floor(util.Mathf.RandomRange(1, 6));
+                    _this._dice2.image = _this._diceArray[_this._dice2Number];
+                    _this._dice2Label.text = _this._dice2Number.toString();
+                    console.log(_this._dice2Number);
+                    // move dices off
+                    _this._moveDices = false;
+                });
             });
             this.Main();
         };
+        /**
+         * This method update the objects according to the FPS
+         *
+         * @memberof Play
+         */
         Play.prototype.Update = function () {
+            if (this._moveDices) {
+                //set a random dice 
+                var dice1 = Math.floor(util.Mathf.RandomRange(1, 6));
+                this._dice1.image = this._diceArray[dice1];
+                var dice2 = Math.floor(util.Mathf.RandomRange(1, 6));
+                this._dice2.image = this._diceArray[dice2];
+            }
         };
+        /**
+         * Main function of the Scene
+         *
+         * @memberof Play
+         */
         Play.prototype.Main = function () {
             this.addChild(this._dice1);
+            this.addChild(this._dice1Label);
             this.addChild(this._dice2);
+            this.addChild(this._dice2Label);
             this.addChild(this._rollBtn);
         };
         return Play;
--- COMP397-W2020-Midterm-Jovane/Scripts/scenes/Play.ts
+++ COMP397-W2020-Midterm-Jovane/Scripts/scenes/Play.ts
@@ -1,13 +1,22 @@
+/**
+ * Source File: Play.ts
+ * Author: Tom Tsiliopoulos / Jovane Marques
+ * Date: Feb 22, 2020
+ * Description: Play Scene of a dice game
+ */
 module scenes
 {
     export class Play extends objects.Scene
     {
         // PRIVATE INSTANCE MEMBERS
         private _dice1:objects.Button;
+        private _dice1Label:objects.Label;
         private _dice2:objects.Button;
+        private _dice2Label:objects.Label;
         private _rollBtn:objects.Button;
         private _dice1Number:number;
         private _dice2Number:number;
+        private _moveDices:boolean = false;
         private _diceArray:Object[] = [
             config.Game.ASSETS.getResult("diceBlank"), 
             config.Game.ASSETS.getResult("dice1"), 
@@ -29,35 +38,78 @@ module scenes
         }
 
         // PRIVATE METHODS
+        /**
+         * Method to wait a period of time before doing some actions
+         *
+         * @private
+         * @param {number} time
+         * @returns
+         * @memberof Play
+         */
+        private sleep (time:number) {
+            return new Promise((resolve) => setTimeout(resolve, time));
+        }
 
         // PUBLIC METHODS
 
-        //initialize and instatiate
+        /**
+         * initialize and instatiate
+         *
+         * @memberof Play
+         */
         public Start(): void 
         {
             this._dice1 = new objects.Button(config.Game.ASSETS.getResult("diceBlank"), 100, 150);
+            this._dice1Label = new objects.Label("0", "30px", void 0, void 0, 200, 360);
             this._dice2 = new objects.Button(config.Game.ASSETS.getResult("diceBlank"), 350, 150);
+            this._dice2Label = new objects.Label("0", "30px", void 0, void 0, 450, 360);
             this._rollBtn = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 250, 400);
             this._rollBtn.on('click', (e) => {
-                this._dice1Number = Math.floor(util.Mathf.RandomRange(1, 6));
-                this._dice1.image = this._diceArray[this._dice1Number] as any;
-                console.log(this._dice1Number);
-                this._dice2Number = Math.floor(util.Mathf.RandomRange(1, 6));
-                this._dice2.image = this._diceArray[this._dice2Number] as any;
-                console.log(this._dice2Number);
+                this._moveDices = true;
+                this.sleep(1000).then(() => {
+                    this._dice1Number = Math.floor(util.Mathf.RandomRange(1, 6));
+                    this._dice1.image = this._diceArray[this._dice1Number] as any;
+                    this._dice1Label.text = this._dice1Number.toString();
+                    console.log(this._dice1Number);
+
+                    this._dice2Number = Math.floor(util.Mathf.RandomRange(1, 6));
+                    this._dice2.image = this._diceArray[this._dice2Number] as any;
+                    this._dice2Label.text = this._dice2Number.toString();
+                    console.log(this._dice2Number);
+
+                    // move dices off
+                    this._moveDices = false;
+                });
+                
             });
             this.Main();
         }        
-        
+        /**
+         * This method update the objects according to the FPS
+         *
+         * @memberof Play
+         */
         public Update(): void 
         {
-
+            if (this._moveDices){
+                //set a random dice 
+                let dice1 = Math.floor(util.Mathf.RandomRange(1, 6));
+                this._dice1.image = this._diceArray[dice1] as any;
+                let dice2 = Math.floor(util.Mathf.RandomRange(1, 6));
+                this._dice2.image = this._diceArray[dice2] as any;
+            }
         }
-        
+        /**
+         * Main function of the Scene
+         *
+         * @memberof Play
+         */
         public Main(): void 
         {
             this.addChild(this._dice1);
+            this.addChild(this._dice1Label);
             this.addChild(this._dice2);
+            this.addChild(this._dice2Label);
             this.addChild(this._rollBtn);
         }
 
